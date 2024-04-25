ffa0b97592a750aa471259acb52b2dbba16828f3
YanaOSuh <yanochkaiv@yahoo.com>
2024-04-25 17:22:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function weddingCountdown() {
+    const weddingDate = new Date("June 22, 2024 12:00");
+    const now = new Date();  
+    const diff = weddingDate - now;
+
+    const msInSecond = 1000;
+    const msInMinute = 60 * 1000;
+    const msInHour = 60 * 60 * 1000;
+    const msInDay = 24 * 60 * 60 * 1000;
+
+    const displayDay = Math.floor(diff/msInDay);
+    document.querySelector(".days").textContent = displayDay;
+
+    const displayHour = Math.floor((diff%msInDay)/msInHour);
+    document.querySelector(".hours").textContent = displayHour;
+
+    const displayMinute = Math.floor((diff%msInHour)/msInMinute);
+    document.querySelector(".minutes").textContent = displayMinute;
+
+    const displaySecond = Math.floor((diff%msInMinute)/msInSecond);
+    document.querySelector(".seconds").textContent = displaySecond;
+
+    if (diff <=0) {
+        document.querySelector(".days").textContent = 0;
+        document.querySelector(".hours").textContent = 0;
+        document.querySelector(".minutes").textContent = 0;
+        document.querySelector(".seconds").textContent = 0;
+        clearInterval(timerID);
+        congrats();
+        updateImage()
+    }
+}
+let timerID = setInterval(weddingCountdown,1000);
